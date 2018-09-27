047741bb726263033e2edb8717b72a60d2deba62
TonyChopper9 <TonyChopper9@users.noreply.github.com>
2018-09-27 13:48:41 +0200

    Merge and save
    Es werden jetzt automatisch alle neuen daten der bestehenden Datenbank hinzugefügt

--- Sprachen.py
+++ Sprachen.py
@@ -1,4 +1,6 @@
 import re
+import csv
+
 
 def text_interpretation(text):
 
@@ -14,11 +16,8 @@ def text_interpretation(text):
     text_noname = set(text_split) - set(_namen)
     #Entfernt die Namen aus dem Text
 
-    #text_noname = list(filter(_namen, text_split))
-    #text_noname = list(filter(lambda x: x not in txtRegex.match, text_split))
-    #text_noname = (x for x in text_split and x not in _namen)
-
     return text_noname
+
 
 def buchstaben_lesen(wlist):
 
@@ -46,11 +45,81 @@ def buchstaben_lesen(wlist):
         return(dictionary)
 
 print()
-#print(text_interpretation(input("Testtext: \n")))
-print(buchstaben_lesen(text_interpretation(input("Testtext: \n"))))
+
+
+def csv_merge(d):
+    try:
+
+        with open('letterdata.csv') as csvfile:
+            ftg = {}
+
+            nd = dict(filter(None, csv.reader(csvfile)))
+            #liest file in dictionary nd
+
+            for x in nd:
+                if nd[x] is not '':
+                    nd[x] = int(nd[x])
+                    #handlet leere zellen und macht die str nummern zu int
+
+                else:
+                    continue
+
+            for x in d:
+                ftg[x] = nd[x] + d[x]
+                #addiert alle zu ändernden zellen in ftg
+
+            nd.update(ftg)
+            #pusht die neuen zellen auf nd
+
+            #print(ftg)
+            return(nd)
+
+
+    except:
+        print('ERROR_LESEN')
+
+
+def csv_speichern(d):
+    #Komma geht noch nicht
+    try:
+
+        with open('letterdata.csv', 'w') as csvfile:
+
+            [csvfile.write('{0},{1}\n'.format(key, value)) for key, value in d.items()]
+
+    except:
+        print("ERROR_SPEICHERN")
 
 
 
+#csv_speichern(buchstaben_lesen(text_interpretation(input("Testtext: \n"))))
+csv_speichern(csv_merge(buchstaben_lesen(text_interpretation(input("Testtext: \n")))))
+
+
+
+
+
+
+
+#Fehlversuche Sammlung:
+#fieldnames = ['letter', 'absolute_frequency']
+
+            #writer = csv.writer(csvfile)
+            #for letter, absolute_frequency in d.items():
+            #writer.writerow(letter, absolute_frequency)
+
+            #columns = ['lttr', 'absf', 'relf']
+            #w = csv.DictWriter(csvfile, d.keys())
+            #w.writerow(d)
+
+#for row in csvfile:
+             #   print(row)
+              #  x, y = row
+               # d[x] += y
+
+#text_noname = list(filter(_namen, text_split))
+    #text_noname = list(filter(lambda x: x not in txtRegex.match, text_split))
+    #text_noname = (x for x in text_split and x not in _namen)
 
 
 
